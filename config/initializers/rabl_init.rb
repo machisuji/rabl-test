f4c9baeffbb00d6d65c32db97a414e9094874019
Markus Kahl <machisuji@googlemail.com>
2013-12-13 12:18:19 +0000

    use default rabl config

--- config/initializers/rabl_init.rb
+++
@@ -1,8 +0,0 @@
-require 'rabl'
-
-Rabl.configure do |config|
-  config.include_json_root = true
-  config.include_xml_root  = false
-  config.include_child_root = false
-  config.json_engine = ::Oj
-end
